d2fefd17a6610bfa4afa563c9755327cc8ad7dba
Kinya <kinyaruthm@gmail.com>
2023-03-07 23:40:03 -0800

    task 0

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import random
-number = random.randint(-10,10)
+<F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11><F11>number = random.randint(-10,10)
 
