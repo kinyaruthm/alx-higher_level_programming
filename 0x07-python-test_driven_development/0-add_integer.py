0a2230585f7a19f1fedf43002974f1fffdb2dc8e
Kinya <kinyaruthm@gmail.com>
2023-04-01 10:52:37 -0700

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+How to Use 0-add_integer.py
+
+This module defines an integer addition function ''add_integr(a, b=98)''.
+
+;;
+>>> add_integer = __import__('o-add_integer').add_integer
+>>> add_integer(2, 3)
+5
+
+;;
+>>> add_integer(2, -3)
+-1
